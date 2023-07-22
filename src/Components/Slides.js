d37f8a45c9738194da6ecea3ed74da5de1c02265
Rafay Zubair Gill <zubairgillrafay@gmail.com>
2023-07-23 00:52:01 +0500

    Website(front-end & back-end) completed

--- src/Components/Slides.js
+++ src/Components/Slides.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import axios from 'axios';
 import {
   MDBBtn,
@@ -10,17 +11,46 @@ import {
 }
 from 'mdb-react-ui-kit';
 
+import Main from './Main';
+
 function Slides({slides}) {
 
-  const [currentSlide, setSlide] = useState(0);
+  const list_bg = ['bg1', 'bg2', 'bg3']
 
-  const download_slides = async () => {
-    try {
-        const response = await axios.get('/download');
-    } catch (error) {
-        console.error('Error fetching image:', error);
-    }
+  const [currentSlide, setSlide] = useState(0);
+  const [currentBg, setBg] = useState(0);
+  const [isResponseReceived, setIsResponseReceived] = useState(false);
+
+  const try_again = () => {
+    setIsResponseReceived(true)
   }
+
+  const next_bg = () => {
+    setBg((currentBg + 1) % list_bg.length)
+  }
+
+  const prev_bg = () => {
+    setBg((currentBg + 5) % list_bg.length)
+  }
+
+  const download_slides = () => {
+    axios({
+      url: 'http://127.0.0.1:5000/download',
+      method: 'POST',
+      responseType: 'blob', // Important: Set the response type to 'blob'
+      data: {template_no: currentBg + 1}
+    }).then((response) => {
+      // Create a URL object from the response data
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      // Create a link element to initiate the download
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', 'presentation.pptx'); // Set the desired filename
+      // Trigger the download by simulating a click on the link
+      document.body.appendChild(link);
+      link.click();
+    });
+  };
 
   return (
     <MDBContainer fluid className='p-4 background-radial-gradient overflow-hidden'>
@@ -30,7 +60,7 @@ function Slides({slides}) {
           <MDBCard className='my-5 bg-glass'>
             <MDBCardBody className='p-5'>
               <div className='main'>
-                  <div id="slide" className="parent">
+                  <div id="slide" className={list_bg[currentBg]}>
                       <div className='float-child'>
                           <div>
                             <h1 data-testid="title">{slides[currentSlide].Heading}</h1>
@@ -44,6 +74,7 @@ function Slides({slides}) {
               </div>
 
               <div id="navigation" className="text-center">
+                {/* IMPORTANT: Add Flex Box for the buttons */}
                   <button
                       data-testid="button-restart"
                       className={currentSlide === 0 ? "small outlined" : "small"}
@@ -69,20 +100,41 @@ function Slides({slides}) {
                       Next
                   </button>
                   <button
+                      onClick={download_slides}
                       data-testid="button-donwload"
                   >
                       Download
                   </button>
                   <button
+                      // onClick={try_again}
                       data-testid="button-try-agian"
                   >
                       Try Again
+                  </button>
+                  <button
+                      onClick={next_bg}
+                      data-testid="button-n"
+                  >
+                      N
+                  </button>
+                  <button
+                      onClick={prev_bg}
+                      data-testid="button-p"
+                  >
+                      P
                   </button>
               </div>
             </MDBCardBody>
           </MDBCard>
         </MDBCol>
       </MDBRow>
+
+
+      {isResponseReceived && <Router>
+        <Switch>
+        <Route path="/" render={(props) => <Main slide={null} />} />
+        </Switch>
+      </Router>}
     </MDBContainer>
   );
 }
