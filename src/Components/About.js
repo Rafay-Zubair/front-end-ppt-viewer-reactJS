dd83a654578e94abdb83fba356b91d03112ff36a
Rafay Zubair Gill <zubairgillrafay@gmail.com>
2023-08-11 22:45:39 +0500

    added Features and About Page. Removed all the redundant lines of code

---
+++ src/Components/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About() {
+    return(
+        <div id="About">
+            About Page
+        </div>
+    )
+}
+
+export default About
---
+++ src/Components/Features.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Features() {
+    return(
+        <div id="Features">
+            Features Page
+        </div>
+    )
+}
+
+export default Features
